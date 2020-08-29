dc95c538d46c59a85b8344b21d2e67c190b681ef
Sluzhynskyi Danylo <sluzhynskyi@gmail.com>
2020-08-29 19:42:18 +0300

    init

---
+++ shell_make.sh
@@ -0,0 +1,90 @@
+#!/usr/bin/env bash
+filename='shmakefile'
+# Transform long options to short ones
+for arg in "$@"; do
+    shift
+    case "$arg" in
+    "--help") set -- "$@" "-h" ;;
+    "--file") set -- "$@" "-f" ;;
+    *) set -- "$@" "$arg" ;;
+    esac
+done
+# Parsing short options
+while getopts ":hf:" opt; do
+    case ${opt} in
+    h)
+        echo "Usage:"
+        echo "    shell_make -h                 Display this help message."
+        echo "    shell_make -f                 Use file as a makefile"
+        exit 0
+        ;;
+    f)
+        filename=$OPTARG
+        ;;
+    \?)
+        echo "Usage: shell_make [-h] [-f makefile]"
+        exit 1
+        ;;
+    :)
+        echo "Usage: shell_make [-f makefile], needs an argument"
+        exit 1
+        ;;
+    esac
+done
+shift $((OPTIND - 1))
+# Creating an array for targets that provided user
+
+declare -A targets
+declare -A builds # Associative array of target:True, if build, target:False, if not
+declare -A commands
+declare -a orders
+
+# Reading file
+IFS=$'\n'
+while read -r line; do
+    # reading each line
+    IFS=':' read -r -a array <<<"$line"
+    if [ ${#array[1]} -gt 0 ]; then
+        orders+=("${array[0]}")
+        target="${array[0]}"
+        dependencies="${array[1]}"
+        targets["$target"]="$dependencies"
+    fi
+
+    if grep -q -P "^\t" <<<"$line"; then
+        commands["$target"]="$line"
+    fi
+done <"$filename"
+
+if [ $# -gt 0 ]; then
+    start_targets=("$@")
+else
+    start_targets=("${orders[0]}")
+#    echo "${start_targets[0]}"
+fi
+
+#for key in "${orders[@]}"; do
+#    echo "$key"
+#    echo "${targets[$key]}"
+#done
+#for key in "${commands[@]}"; do echo "$key"; done
+for key in "${!targets[@]}"; do builds[$key]=false; done
+#for value in "${targets[@]}"; do echo "$value"; done
+IFS=" "
+make() {
+    if [ "${builds[$1]}" = false ]; then
+        builds[$1]=true
+        #    echo "in make"
+        dep="${targets[$1]}" # Get dependencies for target
+        #    read -r -a arr_dep -d '' <<<"$dep" #
+        for d in $dep; do
+            d_t="${d//[$'\t\r\n ']/}"
+            [ -n "$d_t" ] && [ ${targets[$d_t]+abc} ] && make "$d_t"
+        done
+        [ ${commands[$1]+abc} ] && eval "${commands[$1]}"
+    fi
+}
+for t in "${start_targets[@]}"; do
+    ! [ ${targets["$t"]+abc} ] && echo "This target does not exist" && exit 1
+    make "$t"
+done
